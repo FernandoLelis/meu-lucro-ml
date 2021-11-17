a34e0f21849c742806d4cd762751436054443171
FernandoLelis <fernandolelisdarocha@gmail.com>
2021-11-17 17:24:38 -0300

    Adicionado cores no css

--- meu-lucro-ml/src/App.js
+++ meu-lucro-ml/src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  
   return (
     <div className="App">
+      <div className="boxTop">
+        <div className="boxTitle">
+          <h1>
+            Olá, vamos calular seu lucro!
+          </h1>
+        </div>
+        </div>
+      <div className="boxForm">
+        Formulário
+      </div>
       
     </div>
   );
