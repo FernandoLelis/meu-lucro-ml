c63f6c90a98ee2342ee60ff26010cfb1c71725e1
FernandoLelis <fernandolelisdarocha@gmail.com>
2021-12-03 17:50:43 -0300

    Alterações da semana

--- meu-lucro-ml/src/Components/BoxForm.js
+++ meu-lucro-ml/src/Components/BoxForm.js
@@ -2,15 +2,16 @@ import React, { useState } from "react";
 import BoxRes from "./BoxRes";
 import './BoxForm.css';
 import ValorFrete from "./ValorFrete";
+import Categorias from "./Categorias";
 
 const categorias = [
     {
         nome : "Celulares e SmartFones",
-        tipo : ["classico", "premium"]
+        tipo : ["Clássico", "Premium"]
     },
     {
         nome : "Suplementos",
-        tipo : ["classico", "premium"]
+        tipo : ["Clássico", "Premium"],
     }
 ]
 
@@ -19,13 +20,15 @@ export default function BoxForm() {
 
     const [custo, setCusto] = useState('')
 
-    const [idTipo, setIdTipo] = useState(-1);
+    const [selectCat, setSelectCat] = useState(-1);
 
     const handlerCarregarTipo = function (e) {
         const opcao = e.target.value;
 
-        setIdTipo(opcao);
+        setSelectCat(opcao);
     }
+
+    const [tipo, setTipo] = useState("")
 
     return(
         <div>
@@ -40,6 +43,7 @@ export default function BoxForm() {
                     placeholder="Ex: 45.90" 
                     onChange={(ev) => setCusto(ev.target.value)} 
                 />
+                <div className="cat">
                 <label htmlFor="categorias">Categoria:</label>
                 <select name="categorias" id="selCategorias" onClick={handlerCarregarTipo}>
                     <option value={-1}>Selecione</option>
@@ -50,19 +54,32 @@ export default function BoxForm() {
                     }
                 </select>
                 <label htmlFor="tipo">Tipo: </label>
-                <select name="tipo" id="selTipo">
-                    {
-                        idTipo > -1 && 
-                        (
-                            categorias[idTipo].tipo.map((item, i) => (
-                                <option key ={"tipo" + i} value={i}>{item}</option>
-                            ))
+                {
+                    
+                    selectCat == -1 && 
+                    (
+                            <label>
+                            <input type="radio" name="tipo" value=""/> Clássico
+                            <input type="radio" name="tipo" value="" /> Premium
+                            </label>
+                    ) 
+                }
+                {
+                    
+                    selectCat > -1 && 
+                    (
+                        categorias[selectCat].tipo.map((item, i) => (
+                            <label>
+                            <input type="radio" key={"tipo" + i} name="tipo" value={item} onChange={(ev) => setTipo(ev.target.value)} /> {item}
+                            </label>
                         )
-                    }
-                </select>
+
+                    ))
+                }
+                </div>
                 <ValorFrete />
                 
-                {console.log(idTipo)}
+                {console.log(tipo)}
 
 
 
@@ -70,7 +87,8 @@ export default function BoxForm() {
             </form>
             <BoxRes 
                 custo={custo}
-                idTipo={idTipo}
+                idTipo={selectCat}
+                idTipo={tipo}
                 
             />
 
--- meu-lucro-ml/src/Components/Categorias.js
+++ meu-lucro-ml/src/Components/Categorias.js
@@ -1,64 +1,19 @@
-import React, { useState } from "react";
-import './Categorias.css';
-
-
-const categorias = {
-    celularesSmartphones:{
-        classico: 0.11,
-        premium: 0.16 
-    },
-    suplementos:{
-        classico: 0.13,
-        premium: 0.16 
-    }
-};
-
-console.log(categorias)
-
 export default function Categorias(props) {
 
- 
-    const [selectCat, setSelectCat] = useState('');
-
-    const [tipo, setTipo] = useState('');
-
- 
+    const categorias = [
+        {
+            nome : "Celulares e SmartFones",
+            tipo : ["Clássico", "Premium"]
+        },
+        {
+            nome : "Suplementos",
+            tipo : ["Clássico", "Premium"],
+        }
+    ]
 
     return(
-        <div className="cat">
-            <label htmlFor="selectCat">Categoria:</label> <br />
-            <select name="selectCat" id="selectCat" value={selectCat} onChange={(ev) => setSelectCat(ev.target.value)} >
-                <option value="">Selecione</option>
-                <option value="celularesSmartphones">Celulares e Smartphones</option>
-                <option value="suplementos">Suplementos</option>
-                <option value="tenis">Tênis</option>
-                <option value="lumináriasTetoSolaresArandelas">Luminárias de Teto, Solares e Arandelas</option>
-                <option value="notebooks">Notebooks</option>
-                <option value="tratamentosCabelo">Tratamentos para o Cabelo</option>
-                <option value="calcas">Calças</option>
-                <option value="fonesHeadsets">Fones e Headsets</option>
-                <option value="camisolasMoletons">Camisolas e moletons</option>
-                <option value="botas">Botas</option>
-                <option value="bicicletas">Bicicletas</option>
-                <option value="hdsSdds">HDs e SSDs</option>
-                <option value="roteadores">Roteadores</option>
-                <option value="relogiosPulso">Relógios de Pulso</option>
-                <option value="smartwatches">Smartwatches</option>
-                <option value="computadores">Computadores</option>
-                <option value="impressoras">Impressoras</option>
-                <option value="televisores">Televisores</option>
-                <option value="caixasSomAutofalantes">Caixas de Som e Autofalantes</option>
-                 <option value="mediaStreaming">Media Streaming</option>
-            </select>
-
-            <label htmlFor="tipo">Tipo:<br />
-            <input type="radio" name="tipo" id="tipo" value="classico" onChange={(ev) => setTipo(ev.target.value)} /> Clássico
-            <input type="radio" name="tipo" id="tipo" value="Premium" onChange={(ev) => setTipo(ev.target.value)} /> Premium
-        </label>
-        
-        <div>{selectCat}</div>
-        <div>{tipo}</div>
-        <div>{}</div>
+        <div>
+            {props}
         </div>
     )
 }
